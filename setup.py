43b2aa9de61946813b32cd537a4ec56b8bcf53af
Baran Gökalp <61322347+brngklp@users.noreply.github.com>
2024-09-15 15:15:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
    name='frost',
    version='1.0',
-   description='A modern password manager wr,itten in python',
+   description='A modern password manager written in Python',
    author='Baran Gokalp',
    author_email='oxydess@protonmail.com',
-   packages=['frost'],  #same as name
-   install_requires=['argparse'], #external packages as dependencies
+   packages=['frost'],  # Same as the name of the package
+   install_requires=['argparse'],  # external dependencies here if needed
 )
